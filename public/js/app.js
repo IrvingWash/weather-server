06cf0b78e28aa47e3080fbbb2c81399ae412be88
Washington Irving <iamwashingyou@gmail.com>
2022-04-03 16:58:06 +0300

    add client-side weather fetching

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
-console.log('Client side JS file is loaded');
+const weatherForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+
+weatherForm.addEventListener('submit', getWeather);
+
+function getWeather(event) {
+	event.preventDefault();
+
+	const address = searchInput.value;
+
+	if (address === undefined) {
+		console.log('You need to specify an address');
+
+		return;
+	}
+
+	fetch(`http://localhost:3000/weather?address=${address}`)
+		.then((response) => {
+			response.json()
+				.then((data) => {
+					if (data.error) {
+						console.log(data.error);
+
+						searchInput.value = '';
+
+						return;
+					}
+
+					console.log(data.location);
+					console.log(data.forecastData);
+
+					searchInput.value = '';
+				});
+		});
+}
